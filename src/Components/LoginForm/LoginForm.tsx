1466540e5a7b6cb7040d01b38880087853a892cf
Antoine Gorenflot <antoine@ateliom.fr>
2021-04-29 22:35:15 +0200

    Send login to API and receive jwt in front

--- src/Components/LoginForm/LoginForm.tsx
+++ src/Components/LoginForm/LoginForm.tsx
@@ -11,7 +11,9 @@ import {
 } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import * as EmailValidator from 'email-validator'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
+import axios from 'axios'
+import { gql, useQuery } from '@apollo/client'
 import useStyles from './LoginStyle'
 
 interface ILogin {
@@ -26,10 +28,24 @@ const LoginForm: React.FC = () => {
   const [password, setPassword] = useState('')
   const [remember, setRemember] = useState(false)
 
+  const { REACT_APP_API_URL } = process.env
+
   const onSubmit = (): void => {
     const formData: ILogin = { email, password, remember }
+    // Ci-dessous envoi de la query en front, à definir les besoins
+    // du contenu du JWT.
+    // Pour le moment le booleen "remember" n'est pas envoyé au back.
+    const crendentialsToSend = {
+      query: `
+      query {
+        login(input:{email:"${formData.email}", password: "${formData.password}"}){token}}
+      `,
+    }
+    axios
+      .post(`${REACT_APP_API_URL}daddyStuddies`, crendentialsToSend)
+      .then((res) => console.log(res.data))
+    // Token dans la réponse, à envoyer dans un coockie en front ?
   }
-
   const validate = () => {
     return EmailValidator.validate(email) && password.length > 4
   }
